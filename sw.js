4ec6b0abef0de0d6b834a6cc2c1e25a1dff7d78b
zoeykang13 <zoeykang13@gmail.com>
2026-03-29 20:31:30 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'fotm-v1';
+const ASSETS = [
+  './checklist.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
